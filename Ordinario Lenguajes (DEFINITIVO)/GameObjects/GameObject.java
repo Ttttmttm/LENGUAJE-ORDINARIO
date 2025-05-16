5363cbecf90c54ffa9486dd4c1371c82dec9ede0
Ttttmttm <tonatiuhmoralesr@gmail.com>
2025-05-16 14:28:16 -0600

    GameObject.java

--- Ordinario Lenguajes (DEFINITIVO)/GameObjects/GameObject.java
+++ Ordinario Lenguajes (DEFINITIVO)/GameObjects/GameObject.java
@@ -1,3 +1,10 @@
+# Proyecto: Proyecto Aplicativo Final - Ordinario
+# Integrantes:
+# - Fátima Guadalupe Navas Ramírez
+# - Tonatiuh Morales Rueda
+# - Iñigo Anguiano Chavez
+# Fecha de entrega: 20 de mayo de 2025
+
 package gameObjects;
 
 import java.awt.Graphics;
